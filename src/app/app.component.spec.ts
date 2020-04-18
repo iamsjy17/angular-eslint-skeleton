73750467c23230e1fa8b7c75f5588ebb64de02ef
jewooSong <iamsjy0808@gmail.com>
2020-04-18 18:16:26 +0900

    ESLint + Prettier 세팅 및 README.md 수정

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -5,12 +6,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   }));
 
@@ -20,7 +17,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-eslint-skeleton'`, () => {
+  it("should have as title 'angular-eslint-skeleton'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-eslint-skeleton');
@@ -30,6 +27,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-eslint-skeleton app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'angular-eslint-skeleton app is running!'
+    );
   });
 });
